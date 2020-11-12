3e2c9e7e8c86517f09c8f5752dd0cecce626f933
1382357 <ruben.reyesa@e-campus.uab.cat>
2020-11-12 14:19:41 +0100

    Initial Version

---
+++ precache-manifest.61ae9c2004d94c674dc424ea09e49a30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fff8d83a5740574c7d6c",
+    "url": "/css/app.b8967074.css"
+  },
+  {
+    "revision": "fc41e1881e40cfe4c928",
+    "url": "/css/chunk-vendors.aa90b1ee.css"
+  },
+  {
+    "revision": "f38fdcbd51e350c22694c55498c46ac2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff8d83a5740574c7d6c",
+    "url": "/js/app.7d7b32e5.js"
+  },
+  {
+    "revision": "fc41e1881e40cfe4c928",
+    "url": "/js/chunk-vendors.010a7cba.js"
+  },
+  {
+    "revision": "5388c06a3fc0af08f62e326edda41a73",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
